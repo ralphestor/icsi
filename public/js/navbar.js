790e06bbca7e86985d43b1a054fa12acb2973353
Ralph <ralphestor30@gmail.com>
2021-09-22 01:36:28 +0800

    jquery added

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,26 +1,16 @@
 $(document).ready(() => {
-    const burger = $('.burger');
+    const burger = $('#navBtn');
     const navMobile = $('#navMobile');
     const blocker = $('#blocker');
-    
-    const hideFunc = () => {
-        navMobile.removeClass('displayShow');
-        navMobile.addClass('displayHide');
-        blocker.css('display', 'none');
-    }
-    
-    const showFunc = () => {
-        navMobile.removeClass('displayHide');
-        navMobile.addClass('displayShow');
-        blocker.css('display', 'block');
-    }
-    
-    burger[0].addEventListener('click', showFunc);
-    
-    blocker.click(hideFunc);
-    
-    window.addEventListener('resize', () => {
-        hideFunc();
+
+    burger.click(() => {
+        navMobile.toggleClass('displayShow displayHide');
+        blocker.toggleClass('displayShow displayHide');
     });
-    
+
+    blocker.click(() => {
+        navMobile.toggleClass('displayShow displayHide');
+        blocker.toggleClass('displayShow displayHide');
+    })
 });
+
