8c26a6d3f9f7993b401904d5fe85facc09b6ab98
Ralph <ralphestor30@gmail.com>
2021-09-23 03:24:46 +0800

    jquery edit

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,15 +2,31 @@ $(document).ready(() => {
     const burger = $('#navBtn');
     const navMobile = $('#navMobile');
     const blocker = $('#blocker');
+    
+    const setIcon = () => {
+        const isOn = navMobile.hasClass('displayShow');
+
+        if(isOn) {
+            burger.html('x');
+            burger.toggleClass('xHover bHover');
+        } else {
+            burger.html('☰');
+            burger.toggleClass('xHover bHover');
+        }
+    }
+
+    setIcon();
 
     burger.click(() => {
         navMobile.toggleClass('displayShow displayHide');
         blocker.toggleClass('displayShow displayHide');
+        setIcon();
     });
 
     blocker.click(() => {
         navMobile.toggleClass('displayShow displayHide');
         blocker.toggleClass('displayShow displayHide');
+        setIcon();
     })
 
     $(function () {
@@ -19,6 +35,8 @@ $(document).ready(() => {
             nav.toggleClass('scrolled', $(this).scrollTop() > nav.height());
         });
     });
+
+
 
 });
 
