686f26073e4b0457ac04f8297b6bd336663f1e16
Ralph <ralphestor30@gmail.com>
2021-12-16 02:35:42 +0800

    env variable mongoose url

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports.main = async () => {
-    const url = 'mongodb+srv://Mongo_Ralph:12345@cluster0.t4aio.mongodb.net/ArticlesDB?retryWrites=true&w=majority';
+    const url = process.env.MONGOURL;
     try {
         await mongoose.connect(url, {
             useNewUrlParser: true,
